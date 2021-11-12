35c644b7c20a2fbe86a2f7c5ead120567ea40ccf
sumukhbadam <ssbadam@ucsd.edu>
2021-11-12 09:42:23 -0800

    making dataset compatible with pytorch dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -17,6 +17,9 @@ class Emotion():
             self.size[file_no]= (file,data.shape[1])
         self.total_size = len(self.size)
         print("Done loading the {0} class".format(os.path.split(folder_path)[-1]))
+    
+    def __len__(self):
+        return len(self.size)
 
     def sample(self,emotion_samples)->tuple:
         out = random.choices(self.size,k=emotion_samples)
@@ -31,19 +34,30 @@ class Audiodataset():
     '''
     class for reading the subfolder of each emotion
     '''
-    def __init__(self,folder_path) -> None:
-        self.emotions = os.listdir(folder_path)
-        self.emotions.sort()
-        labels = {j:i for i,j in enumerate(self.emotions)}
-        self.emotions = [Emotion(os.path.join(folder_path,i),labels[i]) for i in self.emotions]
+    def __init__(self,folder_path,sample_per_emotion,mapping=None) -> None:
+        self.emotion_class = os.listdir(folder_path)
+        self.emotion_class.sort()
+        if mapping is None:
+            self.labels = {j:i for i,j in enumerate(self.emotion_class)}
+        else:
+            self.labels = mapping
+        
+        self.emotions = [Emotion(os.path.join(folder_path,i),self.labels[i]) for i in self.emotion_class]
+        self.sample_per_emotion =sample_per_emotion
+       
+    def get_mapping(self):
+        return self.labels
 
-    def sample(self,sample_per_emotion) -> tuple:
+    def __len__(self):
+        return sum([len(i) for i in self.emotions])//self.sample_per_emotion
+
+    def __getitem__ (self,idx):
         '''
         return the samples after appending the zero to make
         all samples equal size and it provides the length array
         '''
         def get_samples(obj):
-            return obj.sample(sample_per_emotion)
+            return obj.sample(self.sample_per_emotion)
         with ThreadPool(5) as p:
             t=p.map(get_samples,self.emotions)
         max_len = max([max(i[2]) for i in t])
@@ -53,7 +67,7 @@ class Audiodataset():
             length.extend(i[2])
             labels.extend(i[1])
         data = np.vstack([np.pad(j,[(0,0),(0,max_len-le),(0,0)],'constant',constant_values=(0,0)) for j,le in zip(i[0],i[2]) for i in t])
-        return (data,labels,length)  
+        return (data,np.array(labels),np.array(length))  
 
 
 
